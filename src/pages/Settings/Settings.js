7b9d667c747a4859452f667420026aeabfe095ad
Leonardo Hernandez <leonardo.hernandez@easesolutions.com>
2021-02-16 16:38:17 -0500

    template del avatar listo y anexo de una librería

--- src/pages/Settings/Settings.js
+++ src/pages/Settings/Settings.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import UploadAvatar from "../../components/Settings/UploadAvatar";
 import "./Settings.scss";
 
 export default function Settings(props) {
@@ -9,6 +10,10 @@ export default function Settings(props) {
     return (
         <div className="settings">
             <h1>Hello Settings</h1>
+            <div className="avatar-name">
+                <UploadAvatar user={user}/>
+                <h2>User Name</h2>
+            </div>
         </div>
     );
 }
